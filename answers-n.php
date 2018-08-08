1aab9aa251f31787ffc4d143fca7b741c8ac9888
Bakary <bakisdu95@hotmail.fr>
2018-08-08 15:05:46 +0200

    Script de answers-n.php

---
+++ answers-n.php
@@ -0,0 +1,10 @@
+<?php
+  require('questionZero.php');
+  require('questionDeux.html');
+  require('questionTrois.html');
+  require('questionQuatre.php');
+  require('questionCinq.html');
+  require('questionSix.html');
+  require('questionSept.php');
+  require('questionHuit.html');
+?>
